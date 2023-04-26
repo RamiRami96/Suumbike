e03b4b9f75554ea714caf8d2a99a99ae85f5f1d7
RamiRami96 <86323113+RamiRami96@users.noreply.github.com>
2023-04-27 04:29:37 +0600

    customize success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { styled } from "@mui/system";
+
+const SuccessContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "92vh",
+});
+
+const SuccessText = styled("h2")(({ theme }) => ({
+  color: theme.palette.primary.main,
+  fontSize: "3rem",
+}));
 
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <SuccessContainer>
+      <SuccessText>Success</SuccessText>
+    </SuccessContainer>
+  );
 }
