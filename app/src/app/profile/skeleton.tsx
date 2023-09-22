a4566ebed4d14c213197767d1984d8f07421b812
RamiRami96 <86323113+RamiRami96@users.noreply.github.com>
2023-09-22 15:10:51 +0600

    fix skeleton

--- app/src/app/profile/skeleton.tsx
+++ app/src/app/profile/skeleton.tsx
@@ -1,16 +1,16 @@
 export function Skeleton() {
   return (
-    <div className="flex justify-between items-center py-3 animate-pulse">
-      <div className="w-[60px] sm:w-[140px] md:w-[200px]pl-2 sm:pl-6">
+    <div className="flex justify-between items-center animate-pulse">
+      <div className="w-[60px] sm:w-[140px] md:w-[200px]pl-2 py-3 sm:pl-6 flex align-center">
         <div className="w-10 h-10 rounded-full bg-gray-300"></div>
       </div>
-      <div className="w-[100px] sm:w-[140px] md:w-[200px] px-2 py-3 sm:px-6 h-10 flex align-center">
+      <div className="w-[100px] sm:w-[140px] md:w-[200px] px-2 py-3 sm:px-6 flex align-">
         <div className="w-24 sm:w-24 md:w-48 h-10 bg-gray-300"></div>
       </div>
-      <div className="w-[100px] sm:w-[140px] md:w-[200px] px-2 py-3 sm:px-6 h-10 flex align-center">
+      <div className="w-[100px] sm:w-[140px] md:w-[200px] px-2 py-3 sm:px-6 flex align-center">
         <div className="w-14 sm:w-24 md:w-48 h-10 bg-gray-300"></div>
       </div>
-      <div className="w-[60px] sm:w-[140px] md:w-[200px] py-3 pr-4 sm:pr-6 h-10 flex align-center">
+      <div className="w-[60px] sm:w-[140px] md:w-[200px] py-3 pr-4 sm:pr-6 flex align-center">
         <div className="w-14 sm:w-24 md:w-48 h-10 bg-gray-300"></div>
       </div>
     </div>
